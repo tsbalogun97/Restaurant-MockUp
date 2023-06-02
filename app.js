f2deeb103072daf1aad3cdd2d1e5e94c13994af0
Temitope Balogun <tsbalogun97@yahoo.com>
2023-06-02 04:55:00 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+ let menu = {
+   breakfast: [ "Sausage, eggs & cheese", "french toast/pancakes & scrambled eggs","muffins/bagel with cream cheese"],
+   lunch: ["cheese burger", "chicken & fries", "ribs tips with fries"],
+   Dinner: [ "rice & beans", "steak with rice", "mac & cheese with baby ribs"],
+   Desert: [ "chocolate Sundea", "brownies", "vanilla/caramel ice cream"],
+   
+ };
+
+ let location = {
+  location:["Texas"],
+  location:["New York"],
+  location:["Nigeria"],
+
+  
+
+ };
+ 
+
+
+
+
